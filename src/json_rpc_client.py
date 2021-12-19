8e4b19415f9473edde9606ba9f6941e8186571d6
Praveen Alex Mathew <praveen.mathew92@outlook.com>
2021-12-19 12:42:29 -0700

    Refactor JSON RPC Client

--- src/json_rpc_client.py
+++ src/json_rpc_client.py
@@ -5,6 +5,17 @@ from src.data.error import Error
 
 DEFAULT_URL = 'https://devnet.accumulatenetwork.io/v1'
 
+def __get_result_from_json_rpc_response(response):
+    body = json.loads(response.content)
+    is_error = 'error' in body
+    if(is_error):
+        code = body['error']['code']
+        message = body['error']['message']
+        data = body['error']['data']
+        error = Error(code, message, data)
+        raise error
+    return body['result']
+
 def get_account(acc_url: str, net_url=DEFAULT_URL):
     payload = {
         "jsonrpc": "2.0",
@@ -14,23 +25,17 @@ def get_account(acc_url: str, net_url=DEFAULT_URL):
             "url": acc_url
         }
     }
-    response = json.loads(requests.post(net_url, json=payload).content)
-    is_error = 'error' in response
-    if(is_error):
-        code = response['error']['code']
-        message = response['error']['message']
-        data = response['error']['data']
-        error = Error(code, message, data)
-        raise error
+    response = requests.post(net_url, json=payload)
+    result = __get_result_from_json_rpc_response(response)
 
-    account_type = response['result']['type']
-    account_url = response['result']['data']['url']
-    account_balance = response['result']['data']['balance']
+    account_type = result['type']
+    account_url = result['data']['url']
+    account_balance = result['data']['balance']
     account = Account(account_type, account_url, account_balance)
     return account
 
 
-def faucet(acc_url: str, net_url=DEFAULT_URL):
+def faucet(acc_url: str, net_url=DEFAULT_URL) -> str:
     payload = {
         "jsonrpc": "2.0",
         "id": 0,
@@ -39,15 +44,10 @@ def faucet(acc_url: str, net_url=DEFAULT_URL):
             "url": acc_url
         }
     }
-    response = json.loads(requests.post(net_url, json=payload).content)
-    is_error = 'error' in response
-    if(is_error):
-        code = response['error']['code']
-        message = response['error']['message']
-        data = response['error']['data']
-        error = Error(code, message, data)
-        raise error
-    tx_id = response['result']['data']['txid']
+    response = requests.post(net_url, json=payload)
+    result = __get_result_from_json_rpc_response(response)
+
+    tx_id = result['data']['txid']
     return tx_id
 
 
@@ -58,13 +58,6 @@ def version(net_url=DEFAULT_URL) -> str:
         "method": "version"
     }
     response = requests.post(net_url, json=payload)
-    version = json.loads(response.content)['result']['data']['version']
+    result = __get_result_from_json_rpc_response(response)
+    version = result['data']['version']
     return version
-
-if __name__ == '__main__':
-    # faucet('acc://8142b29062a927f87b2a4cc071bde0a31b912d6569a89e9b/ACME', net_url='https://testnet.accumulatenetwork.io/v1')
-    print(faucet('acc://69a89e9b/ACME', net_url='https://testnet.accumulatenetwork.io/v1'))
-    # faucet('acc://8669bca56b7931ea4af487ac8173468099470e89ba4e5df4/ACME')
-    print(get_account('acc://8142b29062a927f87b2a4cc071bde0a31b912d6569a89e9b/ACME', net_url='https://testnet.accumulatenetwork.io/v1'))
-    print(version())
-    # get_account('acc://8669bca56b7931ea4af487ac8173468099470e89ba4e5df4/ACME')
