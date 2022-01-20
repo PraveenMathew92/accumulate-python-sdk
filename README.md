5cf1322d175b93905590994300b43ec32ae5a82a
Praveen Alex Mathew <praveen.mathew92@outlook.com>
2022-01-20 10:44:15 -0700

    Update tests to use assertEqual

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 Accumulate Blockchain uses the json rpc for communication with the mainnet. [Accumulate API Reference](https://docs.accumulatenetwork.io/accumulate/developers/api/api-reference#token-account) 
 
 The sdk is a wrapper over the same.
+
+## Running the tests
+
+`$ python -m unittest -v test/test_json_rpc_client.py`
+
+The expected output is
+
+```
+test_faucet_fail (test.test_json_rpc_client.JSONRPCTestCase) ... ok
+test_faucet_success (test.test_json_rpc_client.JSONRPCTestCase) ... ok
+test_get_account_fail (test.test_json_rpc_client.JSONRPCTestCase) ... ok
+test_get_account_success (test.test_json_rpc_client.JSONRPCTestCase) ... ok
+test_version_success (test.test_json_rpc_client.JSONRPCTestCase) ... ok
+```
--- test/test_json_rpc_client.py
+++ test/test_json_rpc_client.py
@@ -8,12 +8,12 @@ DEFAULT_URL = 'https://testnet.accumulatenetwork.io/v1'
 class JSONRPCTestCase(unittest.TestCase):
     def test_version_success(self):
         version = json_rpc_client.version(DEFAULT_URL)
-        self.assertEqual(version, "v0.2.1-12-g5e55124")
+        self.assertEqual(version, "v0.3.0")
 
     def test_get_account_success(self):
         user_adi = 'acc://8142b29062a927f87b2a4cc071bde0a31b912d6569a89e9b/ACME'
         account = json_rpc_client.get_account(user_adi, net_url=DEFAULT_URL)
-        self.assertEquals(account.url, user_adi)
+        self.assertEqual(account.url, user_adi)
 
     def test_get_account_fail(self):
         invalid_user_adi = 'acc://8142b29062a92/ACME'
@@ -23,7 +23,7 @@ class JSONRPCTestCase(unittest.TestCase):
     def test_faucet_success(self):
         user_adi = 'acc://8142b29062a927f87b2a4cc071bde0a31b912d6569a89e9b/ACME'
         tx_id = json_rpc_client.faucet(user_adi, net_url=DEFAULT_URL)
-        self.assertEquals(type(tx_id), str)
+        self.assertEqual(type(tx_id), str)
 
     def test_faucet_fail(self):
         invalid_user_adi = 'acc://8142b29062a92/ACME'
